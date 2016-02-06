ddcece9317880ab052edaf31d5110b016dcdaaac
janocl <janocl84@gmail.com>
2016-02-06 02:26:31 -0300

    Se desarrolla la funcion de busqueda

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function(){
+	$('#app-body')
+		.find('form')
+		.submit(function(ev){
+			ev.preventDefault();
+			var busqueda = $(this)
+				.find('input[type="text"]')
+				.val();
+				alert('Se ha buscado: '+busqueda);
+		})
+})
